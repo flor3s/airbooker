1e20b9dda4a974140073d138b860b53847fcaddb
Daniel Flores <d.flores042@gmail.com>
2018-02-22 20:53:56 -0500

    fix mailer

--- app/mailers/passenger_mailer.rb
+++ app/mailers/passenger_mailer.rb
@@ -1,6 +1,6 @@
 class PassengerMailer < ApplicationMailer
 
-  def sucessful_booking(passenger)
+  def successful_booking(passenger)
     @passenger = passenger
 
     mail to: passenger.email, subject: "Your flight confirmation"
